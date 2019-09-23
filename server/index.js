c9e469b46bf9c2265ba4028e272d0845321b219c
CrisRod <crodriguez85@gmail.com>
2019-09-23 14:31:24 -0300

    morgan

--- server/index.js
+++ server/index.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
+app.use(morgan('dev'));
+
 
 app.set('port', process.env.PORT || 4000);
+
 
 app.listen(app.get('port'), () => {
     console.log(`Server listening on localhost:${app.get('port')}`)
