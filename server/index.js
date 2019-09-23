d8594b647768085bbac9e7ef765a51015d1dcf15
CrisRod <crodriguez85@gmail.com>
2019-09-23 14:32:33 -0300

    cors

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 
 const app = express();
 app.use(morgan('dev'));
+app.use(cors());
 
 
 app.set('port', process.env.PORT || 4000);
