3e46fa46b0472393e299a42ce862933c01d9f6b4
CrisRod <crodriguez85@gmail.com>
2019-09-23 14:34:04 -0300

    json

--- server/index.js
+++ server/index.js
@@ -6,9 +6,11 @@ const app = express();
 app.use(morgan('dev'));
 app.use(cors());
 
+// Use formato json
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.set('port', process.env.PORT || 4000);
-
 
 app.listen(app.get('port'), () => {
     console.log(`Server listening on localhost:${app.get('port')}`)
