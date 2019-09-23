28c57719f2395a7f8f62e9ec9cd10940d7eb84a6
CrisRod <crodriguez85@gmail.com>
2019-09-23 14:47:48 -0300

    mongo conexion

--- server/index.js
+++ server/index.js
@@ -1,7 +1,19 @@
 import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+require('dotenv').config();
 
+// Conexion MongoDB
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.MONGO_URI,{
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+})
+.then(mongoose => console.log('Conectado a Mongo'))
+.catch(error => console.error(error))
 const app = express();
 app.use(morgan('dev'));
 app.use(cors());
