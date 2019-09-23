acbaa835cae5e11b9245cf73d7bb661da2846239
CrisRod <crodriguez85@gmail.com>
2019-09-23 14:28:33 -0300

    set port

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const app = express();
 
+app.set('port', process.env.PORT || 4000);
+
 app.listen(3000, () => {
     console.log('Server Listening on port 3000')
 })
