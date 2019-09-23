cc363207515d8f5242b2fe454517c019edf83a8f
CrisRod <crodriguez85@gmail.com>
2019-09-23 15:26:25 -0300

    routing categoria

---
+++ server/routes/categoria.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import categoriaController from '../controllers/CategoriaController';
+const router = express.Router();
+
+router.post('/add', categoriaController.add);
+router.get('/query', categoriaController.query);
+router.get('/list', categoriaController.list);
+router.put('/update', categoriaController.update);
+router.delete('/remove', categoriaController.remove);
+router.put('/activate', categoriaController.activate);
+router.put('/deactivate', categoriaController.deactivate);
+
