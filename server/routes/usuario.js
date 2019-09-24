65bd050021dfad3b65aad90ded4644be83d83f19
CrisRod <crodriguez85@gmail.com>
2019-09-24 12:45:24 -0300

    asegurando las rutas

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -1,14 +1,15 @@
 import express from 'express';
 import usuarioController from '../controllers/UserController';
+import auth from '../middlewares/auth';
 const router = express.Router();
 
-router.post('/add', usuarioController.add);
-router.get('/query', usuarioController.query);
-router.get('/list', usuarioController.list);
-router.put('/update', usuarioController.update);
-router.delete('/remove', usuarioController.remove);
-router.put('/activate', usuarioController.activate);
-router.put('/deactivate', usuarioController.deactivate);
+router.post('/add', auth.verifyAdministrador, usuarioController.add);
+router.get('/query', auth.verifyAdministrador, usuarioController.query);
+router.get('/list', auth.verifyAdministrador, usuarioController.list);
+router.put('/update', auth.verifyAdministrador, usuarioController.update);
+router.delete('/remove', auth.verifyAdministrador, usuarioController.remove);
+router.put('/activate', auth.verifyAdministrador, usuarioController.activate);
+router.put('/deactivate', auth.verifyAdministrador, usuarioController.deactivate);
 router.post('/login', usuarioController.login);
 
 export default router;
