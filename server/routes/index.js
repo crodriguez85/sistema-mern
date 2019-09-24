22a0c5837902119c8a4ade00a98f73c44d73903a
CrisRod <crodriguez85@gmail.com>
2019-09-24 13:14:22 -0300

    rutas persona

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,11 +2,13 @@ import express from 'express';
 import categoriaRouter from './categoria';
 import articuloRouter from './articulo';
 import usuarioRouter from './usuario';
+import personaRouter from './persona';
 
 const router = express.Router();
 
 router.use('/categoria', categoriaRouter);
 router.use('/articulo', articuloRouter);
 router.use('/usuario', usuarioRouter);
+router.use('/usuario', personaRouter);
 
 export default router;
