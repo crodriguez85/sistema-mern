5efc72c23eb67a2b4b182b667da0d6cda463b6fb
CrisRod <crodriguez85@gmail.com>
2019-09-24 09:31:05 -0300

    Login en ruta user

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -9,5 +9,6 @@ router.put('/update', usuarioController.update);
 router.delete('/remove', usuarioController.remove);
 router.put('/activate', usuarioController.activate);
 router.put('/deactivate', usuarioController.deactivate);
+router.post('/login', usuarioController.login);
 
 export default router;
