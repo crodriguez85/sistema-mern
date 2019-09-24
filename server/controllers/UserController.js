123be99242000b915651174f0a6cd5aa9a075645
CrisRod <crodriguez85@gmail.com>
2019-09-24 09:30:04 -0300

    UserController Login

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -117,5 +117,31 @@ export default {
             });
             next(error);
         }
+    },
+
+    login: async (req, res, next) => {
+        try {
+            let user = await models.Usuario.findOne({email: req.body.email});
+            if(user) {
+                // Existe Usuario con Mail
+                let match = await bcrypt.compare(req.body.password, user.password);
+                if (match){
+                    res.json('Password Correcto')
+                } else {
+                    res.status(404).send({
+                        message: 'Password Incorrecto'
+                    })
+                }
+            } else {
+                res.status(404).send({
+                    message: 'No existe el usuario'
+                });
+            }
+        } catch (error){
+            res.status(500).send({
+                message: 'Ocurrio un Error'
+            });
+            next(error);
+        }
     }
 }
