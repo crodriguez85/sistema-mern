8b5422fc896b8a099ae32e5191eeec3e3405058f
CrisRod <crodriguez85@gmail.com>
2019-09-25 15:34:29 -0300

    partiendo app

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import './App.css';
+import AuthService from './services/AuthService';
 
-function App() {
-  return (
-    <div className="App">
-     
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.auth = new AuthService();
+    this.state = {
+      auth: this.auth.isLoggedIn()
+    }
+  }
+  render() { 
+    return ( 
+      <div></div>
+     );
+  }
 }
-
+ 
 export default App;
