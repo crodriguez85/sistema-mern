b11491e111246573b9419f7dbaef837f80932d39
CrisRod <crodriguez85@gmail.com>
2019-09-25 16:15:50 -0300

    Trabajando el login

--- client/src/components/commons/Login.js
+++ client/src/components/commons/Login.js
@@ -1,18 +1,28 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import AuthService from '../../services/AuthService';
+import {Card, CardHeader} from '@material-ui/core';
 
 class Login extends Component {
     constructor(props){
-        super(props){
-
-        }
+        super(props);
+        this.state = {email: '', password: ''};
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.AuthService = new AuthService();
     }
     render() { 
         return ( 
-            <div>
-                Login
-            </div>
+            <Card>
+                <div style={{textAlign: 'center'}}>
+                    <CardHeader title="Acceso al Sistema"/>
+                </div>
+            </Card>
          );
     }
+}
+
+handleChange(){
+    
 }
  
 export default Login;
